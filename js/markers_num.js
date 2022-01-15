c2bbd4c244c6735bfa357f857a2e2e31d20bf6f2
Noooahh <noah.heimann01@gmail.com>
2022-01-15 20:23:40 +0100

    Update markers_num.js

--- js/markers_num.js
+++ js/markers_num.js
@@ -6,8 +6,6 @@
     .bindPopup("<h2>EKZ</h2>");
   L.marker([-5309, 3242], {icon: cardIcon6}).addTo(map)
     .bindPopup("<h2>Eclipse Tower</h2>");
-  L.marker([-4965, 3695], {icon: cardIcon6}).addTo(map)
-    .bindPopup("<h2>GW Ententeich</h2>");
   L.marker([-5389, 4528], {icon: cardIcon6}).addTo(map)
     .bindPopup("<h2>Pferderennbahn</h2>");
   L.marker([-6827, 3541], {icon: cardIcon6}).addTo(map)
@@ -40,13 +38,100 @@
     .bindPopup("<h2>Lifeinvader</h2>");
   L.marker([-5784, 3917], {icon: cardIcon6}).addTo(map)
     .bindPopup("<h2>Gerichtsgebäude</h2>");
-    
+  L.marker([-6213, 3553], {icon: cardIcon6}).addTo(map)
+    .bindPopup("<h2>Letzte alte Pillbox</h2>");
+  L.marker([-4079, 4773], {icon: cardIcon6}).addTo(map)
+    .bindPopup("<h2>Kautschukplantage</h2>");
+  L.marker([-6379, 4563], {icon: cardIcon6}).addTo(map)
+    .bindPopup("<h2>LS Supply</h2>");
+  L.marker([-3289, 3982], {icon: cardIcon6}).addTo(map)
+    .bindPopup("<h2>Ölverarbeiter Sandy</h2>");
+  L.marker([-4143, 2717], {icon: cardIcon6}).addTo(map)
+    .bindPopup("<h2>Totem</h2>");
+  L.marker([-3178, 3910], {icon: cardIcon6}).addTo(map)
+    .bindPopup("<h2>Cunningham Island</h2>");
+  L.marker([-6519, 3765], {icon: cardIcon6}).addTo(map)
+    .bindPopup("<h2>Davis Kirche</h2>");
+  L.marker([-6075, 3042], {icon: cardIcon6}).addTo(map)
+    .bindPopup("<h2>Vespucci PD</h2>");
+  L.marker([-5757, 4015], {icon: cardIcon6}).addTo(map)
+    .bindPopup("<h2>Vinewood Jahrmarkt</h2>");
+  L.marker([-6132, 3134], {icon: cardIcon6}).addTo(map)
+    .bindPopup("<h2>UG Anwesen UCF</h2>");
+  L.marker([-3724, 4150], {icon: cardIcon6}).addTo(map)
+    .bindPopup("<h2>Harmony Apotheke</h2>");
+  L.marker([-3708, 4151], {icon: cardIcon6}).addTo(map)
+    .bindPopup("<h2>Zivi Paintballhalle</h2>");
+  L.marker([-5322, 2895], {icon: cardIcon6}).addTo(map)
+    .bindPopup("<h2>Richman Hotel</h2>");
+  L.marker([-6188, 2710], {icon: cardIcon6}).addTo(map)
+    .bindPopup("<h2>FIB Dealertipp</h2>");
+  L.marker([-6165, 4234], {icon: cardIcon6}).addTo(map)
+    .bindPopup("<h2>Alte Schneiderei</h2>");
+  L.marker([-5994, 3241], {icon: cardIcon6}).addTo(map)
+    .bindPopup("<h2>Little Seoul Kirche</h2>");
+  L.marker([-6958, 4122], {icon: cardIcon6}).addTo(map)
+    .bindPopup("<h2>LS Company</h2>");
+  L.marker([-7623, 4105], {icon: cardIcon6}).addTo(map)
+    .bindPopup("<h2>Merryweather Halle</h2>");
+  L.marker([-5320, 3112], {icon: cardIcon6}).addTo(map)
+    .bindPopup("<h2>Gärtner</h2>");
+  L.marker([-3264, 5101], {icon: cardIcon6}).addTo(map)
+    .bindPopup("<h2>Sandy Kreisel</h2>");
+  L.marker([-5141, 4121], {icon: cardIcon6}).addTo(map)
+    .bindPopup("<h2>Vinewood Bowl</h2>");
+  L.marker([-1703, 3307], {icon: cardIcon6}).addTo(map)
+    .bindPopup("<h2>Hounds of Hell</h2>");
+  L.marker([3518, 5069], {icon: cardIcon6}).addTo(map)
+    .bindPopup("<h2>Lost MC</h2>");
+  L.marker([-4970, 2754], {icon: cardIcon6}).addTo(map)
+    .bindPopup("<h2>Yakuza</h2>");
+  L.marker([-5699, 2729], {icon: cardIcon6}).addTo(map)
+    .bindPopup("<h2>Bosozoku</h2>");
+  L.marker([-4777, 4658], {icon: cardIcon6}).addTo(map)
+    .bindPopup("<h2>Altes Triaden Anwesen</h2>");
+  L.marker([-5380, 3441], {icon: cardIcon6}).addTo(map)
+    .bindPopup("<h2>ICA</h2>");
+  L.marker([-5406, 3110], {icon: cardIcon6}).addTo(map)
+    .bindPopup("<h2>Hustlers</h2>");
+  L.marker([-6805, 3822], {icon: cardIcon6}).addTo(map)
+    .bindPopup("<h2>Ballas</h2>");
+  L.marker([-6491, 3737], {icon: cardIcon6}).addTo(map)
+    .bindPopup("<h2>Grove</h2>");
+  L.marker([-7491, 4293], {icon: cardIcon6}).addTo(map)
+    .bindPopup("<h2>Midnight</h2>");
+  L.marker([-1312, 3743], {icon: cardIcon6}).addTo(map)
+    .bindPopup("<h2>Falcone</h2>");
+  L.marker([-4688, 4270], {icon: cardIcon6}).addTo(map)
+    .bindPopup("<h2>Panthers</h2>");
+  L.marker([-5774, 2317], {icon: cardIcon6}).addTo(map)
+    .bindPopup("<h2>Deadlock</h2>");
+  L.marker([-7298, 3555], {icon: cardIcon6}).addTo(map)
+    .bindPopup("<h2>Volky</h2>");
+  L.marker([-4534, 2763], {icon: cardIcon6}).addTo(map)
+    .bindPopup("<h2>Kanuverleih</h2>");
+  L.marker([-1350, 4737], {icon: cardIcon6}).addTo(map)
+    .bindPopup("<h2>Pennerpark</h2>");
+  L.marker([-5867, 2984], {icon: cardIcon6}).addTo(map)
+    .bindPopup("<h2>Moviepark</h2>");
+  L.marker([-5136, 2460], {icon: cardIcon6}).addTo(map)
+    .bindPopup("<h2>Altes Bosoanwesen UG</h2>");
+  L.marker([-5100, 1800], {icon: cardIcon6}).addTo(map)
+    .bindPopup("<h2>Altes Bosoanwesen Westhighway</h2>");
+  L.marker([-5969, 2808], {icon: cardIcon6}).addTo(map)
+    .bindPopup("<h2>Altes Bosoanwesen Pier</h2>");
+  L.marker([-6330, 3717], {icon: cardIcon6}).addTo(map)
+    .bindPopup("<h2>Alter Schwarzmarkt</h2>");
+  L.marker([-6278, 4350], {icon: cardIcon6}).addTo(map)
+    .bindPopup("<h2>Neuer Schwarzmarkt</h2>");
+  L.marker([-6168, 3437], {icon: cardIcon6}).addTo(map)
+    .bindPopup("<h2>weazle news baustelle</h2>");
 
- 
  /*L.marker([COORDS], {icon: cardIcon6}).addTo(map)
     .bindPopup("<h2>NAME</h2><p><a href='LINK ZUM BILD' data-lightbox='image-1' data-title='NAME'>Screenshot öffnen</a><div class='input-group input-group-sm mb-3'></div></div></p>");
 
-  
+  L.marker([], {icon: cardIcon6}).addTo(map)
+    .bindPopup("<h2></h2>");
   1 Gelb 
   2 Gruen
   3 Blau
