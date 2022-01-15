1d801f4a8f86cfd3badad5f2de65b2533ab9bf80
Noooahh <noah.heimann01@gmail.com>
2022-01-15 16:57:58 +0100

    Update markers_num.js

--- js/markers_num.js
+++ js/markers_num.js
@@ -1,5 +1,46 @@
   L.marker([-3161, 4665], {icon: cardIcon6}).addTo(map)
     .bindPopup("<h2>Ace Bar</h2>");
+  L.marker([-6314, 2548], {icon: cardIcon6}).addTo(map)
+    .bindPopup("<h2>Shishabar</h2>");
+  L.marker([-5348, 2276], {icon: cardIcon6}).addTo(map)
+    .bindPopup("<h2>EKZ</h2>");
+  L.marker([-5309, 3242], {icon: cardIcon6}).addTo(map)
+    .bindPopup("<h2>Eclipse Tower</h2>");
+  L.marker([-5081, 3571], {icon: cardIcon6}).addTo(map)
+    .bindPopup("<h2>GW Ententeich</h2>");
+  L.marker([-5389, 4528], {icon: cardIcon6}).addTo(map)
+    .bindPopup("<h2>Pferderennbahn</h2>");
+  L.marker([-6827, 3541], {icon: cardIcon6}).addTo(map)
+    .bindPopup("<h2>Ballas Stadion</h2>");
+  L.marker([-7030, 3484], {icon: cardIcon6}).addTo(map)
+    .bindPopup("<h2>Auto Verschrotter</h2>");
+  L.marker([-6090, 3156], {icon: cardIcon6}).addTo(map)
+    .bindPopup("<h2>Chinadenkmal</h2>");
+  L.marker([-2709, 2278], {icon: cardIcon6}).addTo(map)
+    .bindPopup("<h2>Hookies</h2>");
+  L.marker([-6671, 3348], {icon: cardIcon6}).addTo(map)
+    .bindPopup("<h2>Schrottplatz Davis</h2>");
+  L.marker([-1410, 3708], {icon: cardIcon6}).addTo(map)
+    .bindPopup("<h2>Hühnchen Fabrik</h2>");
+  L.marker([-1167, 3823], {icon: cardIcon6}).addTo(map)
+    .bindPopup("<h2>Paleto LSC</h2>");
+  L.marker([-2822, 5238], {icon: cardIcon6}).addTo(map)
+    .bindPopup("<h2>Alte East Highway Garage / Silos</h2>");
+  L.marker([-5977, 4035], {icon: cardIcon6}).addTo(map)
+    .bindPopup("<h2>Mirrorpark Railyard</h2>");
+  L.marker([-3125, 5731], {icon: cardIcon6}).addTo(map)
+    .bindPopup("<h2>Human Labs</h2>");
+  L.marker([-5374, 3928], {icon: cardIcon6}).addTo(map)
+    .bindPopup("<h2>Staatsbank</h2>");
+  L.marker([-6075, 2896], {icon: cardIcon6}).addTo(map)
+    .bindPopup("<h2>Vespucci Bank</h2>");
+  L.marker([-5721, 3286], {icon: cardIcon6}).addTo(map)
+    .bindPopup("<h2>Juwelier</h2>");
+  L.marker([-5689, 3051], {icon: cardIcon6}).addTo(map)
+    .bindPopup("<h2>Lifeinvader</h2>");
+  L.marker([-5808, 3801], {icon: cardIcon6}).addTo(map)
+    .bindPopup("<h2>Gerichtsgebäude</h2>");
+    
 
  
  /*L.marker([COORDS], {icon: cardIcon6}).addTo(map)
